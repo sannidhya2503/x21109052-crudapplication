cfb420a2a7d4785b718722079afd7d0ecd11fb19
sannidhya2503 <100199656+sannidhya2503@users.noreply.github.com>
2022-04-24 22:52:47 +0100

    upload files

--- Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\sanni\Downloads\djangocrudoperation\django-crud-operation\env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()

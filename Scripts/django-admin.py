23b61164857538646cfd506d530c7001feaa7403
sannidhya2503 <100199656+sannidhya2503@users.noreply.github.com>
2022-04-24 22:37:48 +0100

    Add files via upload

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\sanni\Downloads\djangocrudoperation\django-crud-operation\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
